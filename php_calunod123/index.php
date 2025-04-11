5c55d7fb32c090552057cde8d8c94901ac7881a1
April joy Calunod <163228501+ajsnooze123@users.noreply.github.com>
2025-04-10 21:44:29 -0700

    Add files via upload

---
+++ php_calunod123/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = "April joy D. Calunod";
+$age = "21 years old";
+$Address = "Mobod Purok 2 Oroquieta  City";
+
+echo "Hi my name is {$name} April joy Calunod<br>I'm {age} 21 years old <br> {address} i live in Mobod purok-2 oroquieta city";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset= "UTF-8">
+        <meta name="viewport" content="width=, initial-scale=1.0">
+        <title>Document<
+    </head>
+
+
+
